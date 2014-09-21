9dab2f1bbca2a6139b354ab9f3cbc7c79bf8f5c2
paul <akwangho@gmail.com>
2014-09-22 00:32:28 +0800

    Add output log

--- example/examplelibrary.js
+++ example/examplelibrary.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var fs = require('promised-io/fs'),
-    assert = require('assert');
+    assert = require('assert'),
+    Return = require('../lib/robotremote').Return,
+    Logger = require('../lib/robotremote').Logger;
 
 var lib = module.exports;
 
@@ -28,6 +30,33 @@ lib.countItemsInDirectory = function (path) {
 lib.countItemsInDirectory.doc = 'Returns the number of items in the directory specified by `path`.';
 
 /**
+ * Example of asynchronous keyword with log output.
+ *
+ * You can implement asynchronous keywords just returning an A+ promise.
+ * Promise can be resolved or rejected with respectively:
+ *
+ * - {Return} Return value consists of return value as the first param and output log as the second param.
+ * - arbitrary return value, or
+ * - an instance of `Error` if the keyword failed
+ *
+ * Just count items in given directory.
+ *
+ * @param path directory path to count item in with output log in robot log.
+ */
+lib.countItemsInDirectoryWithOutput = function (path) {
+    var logger = new Logger();
+    logger.info('Start to read directory from path[%s].', path);
+    return fs.readdir(path).then(
+        function (items) {
+            logger.debug('The items: [%s].', items.toString());
+            return new Return(items.length, logger.getMsg());
+        });
+};
+// The doc attribute is used for inspection on the command line of client and doc generation.
+// It's optional and defaults to empty string when missing.
+lib.countItemsInDirectoryWithOutput.doc = 'Returns the number of items in the directory specified by `path` with log output.';
+
+/**
  * Example synchronous keyword.
  *
  * Any keyword which does not return an A+ promise is considered sync.
@@ -45,6 +74,20 @@ lib.stringsShouldBeEqual = function (str1, str2) {
     console.log('Comparing \'%s\' to \'%s\'', str1, str2);
     assert.equal(str1, str2, 'Given strings are not equal');
 };
+
+/**
+ *
+ * Example of fail case with log output
+ *
+ * @returns {Return}
+ */
+lib.awfulKeyword = function () {
+    var logger = new Logger();
+    logger.info('Enter awful keyword.');
+    logger.warn('Awful thing is going to happen.');
+    return new Return('Awful return value', logger.getMsg(), new Error('Error happens because this is an awful keyword'));
+};
+lib.awfulKeyword.doc = 'This keyword will cause some terrible thing happen, please use this keyword carefully.';
 
 
 // Run this keyword library if the library itself is called explicitly.
